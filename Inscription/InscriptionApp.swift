b75061e49066b3b1acb0545fabe7e23a2853d4d5
BALDE Thierno <ibrahimabotore@gmail.com>
2021-02-17 10:23:20 +0100

    Initial Commit

---
+++ Inscription/InscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InscriptionApp.swift
+//  Inscription
+//
+//  Created by LPWAM01 on 17/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct InscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
